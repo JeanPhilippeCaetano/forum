6d6193e258e77a511ed24b91c2cd2e0ba0e3d38d
JCAETANO <jeanphilippe.caetano@ynov.com>
2022-06-01 00:14:17 +0200

    database get data & insert data done

--- forum/database/database.go
+++ forum/database/database.go
@@ -2,12 +2,16 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"reflect"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type Users struct {
+	UserID     int
 	Pseudonyme string
 	Email      string
 	Password   string
@@ -28,55 +32,106 @@ type Comments struct {
 	ReceiverID int
 }
 
+type Posts struct {
+	PostID   int
+	SenderID int
+	Title    string
+	Content  string
+}
+
 func InitDatabase() *sql.DB {
 	db, err := sql.Open("sqlite3", "./forum.db")
+	statement := `PRAGMA foreign_keys = ON;
+	CREATE TABLE IF NOT EXISTS users
+	(
+		UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+    	Pseudonyme TEXT NOT NULL UNIQUE,
+    	Email TEXT NOT NULL UNIQUE,
+    	Password TEXT NOT NULL,
+    	Image TEXT NOT NULL
+	);
+	CREATE TABLE IF NOT EXISTS postcomments
+	(
+		CommentID INTEGER PRIMARY KEY AUTOINCREMENT,
+		Content TEXT NOT NULL,
+		SenderID INTEGER NOT NULL,
+		PostID INTEGER NOT NULL,
+		FOREIGN KEY(SenderID) REFERENCES users(UserID),
+		FOREIGN KEY(PostID) REFERENCES posts(PostID)
+	);
+	CREATE TABLE IF NOT EXISTS posts
+	(
+		PostID INTEGER PRIMARY KEY AUTOINCREMENT,
+		SenderID INTEGER NOT NULL,
+		Title TEXT NOT NULL,
+		Content TEXT NOT NULL,
+		FOREIGN KEY(SenderID) REFERENCES users(UserID)
+	);
+	CREATE TABLE IF NOT EXISTS comments
+	(
+		CommentID INTEGER PRIMARY KEY AUTOINCREMENT,
+		Content TEXT NOT NULL,
+		SenderID INTEGER NOT NULL,
+		ReceiverID INTEGER NOT NULL,
+		FOREIGN KEY(SenderID) REFERENCES users(UserID),
+		FOREIGN KEY(ReceiverID) REFERENCES users(UserID)
+	);
+	`
+	db.Exec(statement)
 	if err != nil {
 		log.Panic(err)
 	}
-	statement := `PRAGMA foreign_keys = ON;
-				CREATE TABLE IF NOT EXISTS users(
-					UserID INTEGER PRIMARY KEY AUTOINCREMENT,
-					Pseudonyme UNIQUE TEXT NOT NULL,
-					Email UNIQUE TEXT NOT NULL,
-					Password TEXT NOT NULL,
-					Image TEXT NOT NULL
-				);
-				CREATE TABLE IF NOT EXISTS postcomments(
-					CommentID INTEGER PRIMARY KEY AUTOINCREMENT,
-					Content TEXT NOT NULL,
-					SenderID INTEGER NOT NULL,
-					PostID INTEGER NOT NULL
-					FOREIGN KEY(SenderID)
-						REFERENCES users(UserID)
-				);
-				CREATE TABLE IF NOT EXISTS posts(
-					PostID INTEGER PRIMARY KEY AUTOINCREMENT,
-					SenderID INTEGER NOT NULL,
-					Title TEXT NOT NULL,
-					Content TEXT NOT NULL,
-					FOREIGN KEY(SenderID)
-						REFERENCES users(UserID)
-				);
-				CREATE TABLE IF NOT EXISTS comments(
-					CommentID INTEGER PRIMARY KEY AUTOINCREMENT,
-					Content TEXT NOT NULL,
-					SenderID INTEGER NOT NULL,
-					ReceiverID INTEGER NOT NULL,
-					FOREIGN KEY(SenderID)
-						REFERENCES users(UserID),
-					FOREIGN KEY(ReceiverID)
-						REFERENCES users(UserID)
-				);
-
-	`
-
-	db.Exec(statement)
-
 	return db
 }
 
-type Posts struct {
-	PostID  int
-	Title   string
-	Content string
+func parseParams(structure interface{}, table string, parameters ...string) string {
+	data := reflect.ValueOf(structure)
+	statement := "INSERT INTO " + table
+	attributes := "("
+	values := "VALUES("
+	for i := 1; i < data.NumField(); i++ {
+		attributes += data.Type().Field(i).Name
+		values += "'" + parameters[i-1] + "'"
+		if i != data.NumField()-1 {
+			attributes += ","
+			values += ","
+		}
+	}
+	attributes += ") "
+	values += ")"
+	return statement + attributes + values
+}
+
+func InsertData(structure interface{}, db *sql.DB, table string, parameters ...string) sql.Result {
+	statement := parseParams(structure, table, parameters...)
+	fmt.Println(statement)
+	result, err := db.Exec(statement)
+	if err != nil {
+		log.Panic(err)
+	}
+	return result
+}
+
+func GetAllDataFromTable(db *sql.DB, table string) *sql.Rows {
+	statement := "SELECT * FROM " + table
+	query, err := db.Query(statement)
+	if err != nil {
+		log.Panic(err)
+	}
+	return query
+}
+
+func GetIDNameFromStruct(structure interface{}) string {
+	data := reflect.ValueOf(structure)
+	return data.Type().Field(0).Name
+}
+
+func GetDataFromTableWithID(structure interface{}, db *sql.DB, table string, id int) *sql.Rows {
+	idName := GetIDNameFromStruct(structure)
+	statement := "SELECT * FROM " + table + "WHERE " + idName + "=" + strconv.Itoa(id)
+	query, err := db.Query(statement)
+	if err != nil {
+		log.Panic(err)
+	}
+	return query
 }
