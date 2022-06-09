880831e8b387501f76708818cdd01d6a43d60e8d
Emile <emile.seguret@ynov.com>
2022-06-09 12:14:10 +0200

    push en cours pagination

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,12 +1,3 @@
-const paginationPosts = {
-    maxPosts: 50,
-    initialCount : 1,
-    endCount: 4,
-    page: 1,
-    // initialCount : page * postsPerPage - postsPerPage,
-    // endCount : initialCount + postsPerPage - 1
-}
-
 const addChoosed = (value) => {
     const tags = document.querySelector(".tags-filters")
     tags.childNodes.forEach(elem => {
@@ -66,72 +57,124 @@ tinymce.init({
 
 /* Pagination */
 
+const postsData = {
+    maxPosts: 50,
+    count: 3,
+    sizePagination: 3,
+    page : 1
+}
+
 const pagination = () => {
     // const slider = document.querySelector(".display-pages");
     const slides = document.querySelectorAll(".slide");
     const button = document.querySelectorAll(".arrow");
 
     let current = 0;
-    let prev = (slides.length-1);
+    let prev = (slides.length);
     let next = 1;
     
+    /* variables at the beginning */
+    let startPrev = slides.length
+    let startCurrent = 0
+    let startNext = 1
+
+    let endPrev = slides.length - 1
+    let endCurrent = slides.length
+    let endNext = 0
+    /* beginning pagination */
+    if (slides[0].innerHTML == 1) {
+        pageBtn(startCurrent)
+        exactPageBtn(startPrev, startCurrent, startNext)
+    } else if ((slides[5].innerHTML == postsData.maxPosts) && (slides[4].innerHTML == postsData.maxPosts - 1)) {
+        pageBtn(endCurrent)
+        exactPageBtn(endPrev, endCurrent, endNext)
+    } else {
+        pageBtn(current)
+        exactPageBtn(prev, current, next)
+    }
 
     /* Change active page by clicking on Previous / Next btn */
-    for (let i = 0; i < button.length; i++) {
-        button[i].addEventListener("click", () => {
-            i == 0 ? gotoPrev() : gotoNext();
-            changeTextContent()
-        })
-    }
-
-
-    for (let i = 0; i < slides.length; i++) {
-        /* Change active page by clicking on exact page*/
-        slides[i].addEventListener("click", () => {
-            slides.forEach(elem => {
-                elem.classList.remove("pagination:active")
+    const pageBtn = (current) => {
+        for (let i = 0; i < button.length; i++) {
+            button[i].addEventListener("click", () => {
+                i == 0 ? goTo("prev", current) : gotoNext("next", current);
             })
-            current = i
-            if (current == (slides.length - 1)) {
-                prev = current - 1
-                next = 0
-            }
-            if (current == 0) {
-                prev = slides.length - 1;
-                next = 1
-            }
-            slides[i].classList.add("pagination:active")
-        })
+        }
+    }
+
+    const exactPageBtn = (prev, current, next) => {
+        for (let i = 0; i < slides.length - 1; i++) {
+            /* Change active page by clicking on exact page*/
+            slides[i].addEventListener("click", () => {
+                slides.forEach(elem => {
+                    elem.classList.remove("pagination:active")
+                })
+                current = i
+                if (current == (slides.length - 2)) {
+                    prev = current - 1
+                    next = 0
+                }
+                if (current == 0) {
+                    prev = slides.length - 1;
+                    next = 1
+                }
+                slides[i].classList.add("pagination:active")
+            })
+        }
     }
     
-    const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
-    
-    const gotoNext = () => current < slides.length -1 ? gotoNum(current + 1) : gotoNum(0);
+    const goTo = (prevOrNext, current) => {
+        const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 2);
+        const gotoNext = () => current < slides.length -2 ? gotoNum(current + 1) : gotoNum(0);
+        if (prevOrNext == "prev") {
+            gotoPrev()
+        } else {
+            gotoNext()
+        }
+    }
     
     const gotoNum = number => {
         current = number;
         prev = current - 1;
         next = current + 1;
-        for (let i = 0; i < slides.length; i++) {
+        for (let i = 0; i < slides.length-1; i++) {
             slides[i].classList.remove("pagination:active");
             slides[i].classList.remove("before");
             slides[i].classList.remove("after");
         }
-        if (next == (slides.length)) {
+        if (next == (slides.length-1)) {
             next = 0;
         }
         if (prev == -1) {
-            prev = slides.length - 1;
+            prev = slides.length - 2;
         }
-        slides[current].classList.add("pagination:active");
-        slides[prev].classList.add("before");
-        slides[next].classList.add("after");
+        addClass(prev, current, next)
+        changeInnerSlides(number)
+    }
+
+    const changeInnerSlides = (prev, curr, next) => {
+        if (curr + 1 == 6) {
+            slides.forEach(elem => {
+                elem.classList.remove("pagination:active")
+            })
+            addClass(prev, curr, next)
+            for (let i = 0; i < slides.length-1; i++) {
+                slides[i].innerHTML = i + (paginationPosts.sizePagination * paginationPosts.page)
+            }
+            paginationPosts.endCount += paginationPosts.count
+            paginationPosts.page += 1
+        }
+    }
+    
+    const addClass = (prev, curr, next) => {
+        slides[prev].classList.add("before")
+        slides[curr].classList.add("pagination:active")
+        slides[next].classList.add("after")
     }
 }
 
-const changeInnerSlides = () => {
-    
-}
+
+
 
 pagination()
 
